98efee7bc7fec23ad756b11fc40641ef060d501c
alphanication <usa.alphanication@gmail.com>
2022-05-23 13:21:42 +0300

    setupNavHost, create composable Screens

---
+++ app/src/main/java/com/example/compmoviedb/screens/DetailsScreen.kt
@@ -0,0 +1,9 @@
+package com.example.compmoviedb.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun DetailsScreen(navController: NavController, movieId: Int) {
+
+}
