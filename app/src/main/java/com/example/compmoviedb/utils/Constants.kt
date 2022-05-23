061c04057b8bfc4c08317fa720d8fdbb462defde
alphanication <usa.alphanication@gmail.com>
2022-05-23 12:49:52 +0300

    add Constants Screens

---
+++ app/src/main/java/com/example/compmoviedb/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.compmoviedb.utils
+
+object Constants {
+    object Screens {
+        const val SPLASH_SCREEN = "splash_screen"
+        const val MAIN_SCREEN = "main_screen"
+        const val DETAILS_SCREEN = "details_screen"
+    }
+}
