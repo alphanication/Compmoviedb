69bc8642195de90369b5ba41165f842d78ec0af2
alphanication <usa.alphanication@gmail.com>
2022-05-24 13:42:54 +0300

    change livedata to stateflow mainviewmodel, delete dependencies livedata gradle app

--- app/src/main/java/com/example/compmoviedb/presentation/screens/main/MainViewModel.kt
+++ app/src/main/java/com/example/compmoviedb/presentation/screens/main/MainViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.compmoviedb.presentation.screens.main
 
+import androidx.compose.runtime.MutableState
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -9,6 +10,9 @@ import com.example.compmoviedb.domain.models.Response
 import com.example.compmoviedb.domain.usecase.GetListMoviesPopularUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -17,21 +21,21 @@ class MainViewModel @Inject constructor(
     private val getListMoviesPopularUseCase: GetListMoviesPopularUseCase
 ) : ViewModel() {
 
-    private val _loadingListMoviesPopular = MutableLiveData<Boolean>()
-    val loadingListMoviesPopular: LiveData<Boolean> = _loadingListMoviesPopular
+    private val _loadingListMoviesPopular = MutableStateFlow<Boolean>(false)
+    val loadingListMoviesPopular = _loadingListMoviesPopular.asStateFlow()
 
-    private val _listMoviesPopular = MutableLiveData<List<MoviePopularDetails>>()
-    val listMoviesPopular: LiveData<List<MoviePopularDetails>> = _listMoviesPopular
+    private val _listMoviesPopular = MutableStateFlow<List<MoviePopularDetails>>(listOf())
+    val listMoviesPopular = _listMoviesPopular.asStateFlow()
 
     fun getListMoviesPopular() {
         viewModelScope.launch(Dispatchers.IO) {
             getListMoviesPopularUseCase.execute().collect { result ->
                 when (result) {
-                    is Response.Loading -> _loadingListMoviesPopular.postValue(true)
+                    is Response.Loading -> _loadingListMoviesPopular.emit(true)
                     is Response.Fail -> {}
                     is Response.Success -> {
-                        _loadingListMoviesPopular.postValue(false)
-                        _listMoviesPopular.postValue(result.data.moviesPopularDetails)
+                        _loadingListMoviesPopular.emit(false)
+                        _listMoviesPopular.emit(result.data.moviesPopularDetails)
                     }
                 }
             }
