7c0c95d4d62ab5883987d0817bfc209994da055b
alphanication <usa.alphanication@gmail.com>
2022-05-25 19:17:21 +0300

    GetMovieDetailsByIdUseCase() di settings

--- app/src/main/java/com/example/compmoviedb/di/DomainModule.kt
+++ app/src/main/java/com/example/compmoviedb/di/DomainModule.kt
@@ -2,6 +2,7 @@ package com.example.compmoviedb.di
 
 import com.example.compmoviedb.domain.repository.MoviesRepository
 import com.example.compmoviedb.domain.usecase.GetListMoviesPopularUseCase
+import com.example.compmoviedb.domain.usecase.GetMovieDetailsByIdUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -13,5 +14,10 @@ class DomainModule {
     @Provides
     fun providesGetListMoviesPopularUseCase(moviesRepository: MoviesRepository) : GetListMoviesPopularUseCase {
         return GetListMoviesPopularUseCase(moviesRepository = moviesRepository)
+    }
+
+    @Provides
+    fun providesGetMovieDetailsByIdUseCase(moviesRepository: MoviesRepository) : GetMovieDetailsByIdUseCase {
+        return GetMovieDetailsByIdUseCase(moviesRepository = moviesRepository)
     }
 }
