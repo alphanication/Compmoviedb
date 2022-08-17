bfe32d85ccc9dfe21c6a669f2ae23967930b9ca0
vorobeye <russianfederation.vorobey@gmail.com>
2022-08-17 18:52:17 +0300

    divides DataModule: NetworkModule, NetworkCommonModule

--- app/src/main/java/com/example/compmoviedb/di/NetworkModule.kt
+++ app/src/main/java/com/example/compmoviedb/di/NetworkModule.kt
@@ -9,22 +9,11 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
-@InstallIn(SingletonComponent::class)
 @Module
-class DataModule {
-
-    @Provides
-    @Singleton
-    fun provideRetrofit(): MovieDBApiInterface =
-        Retrofit.Builder()
-            .baseUrl(MovieDBApiInterface.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(MovieDBApiInterface::class.java)
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
 
     @Provides
     @Singleton
